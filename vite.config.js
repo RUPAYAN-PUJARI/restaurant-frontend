9cc06e55997798ab5e06914eaff738a45ec368e2
Rupayan Pujari <rupayanpujari308@gmail.com>
2025-08-03 11:19:46 +0530

    third commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  preview: {
+    host: "0.0.0.0",
+    port: process.env.PORT || 5173,
+    allowedHosts: ["restaurant-frontend-8nhe.onrender.com"], // ✅ Add this line
+  },
+});
